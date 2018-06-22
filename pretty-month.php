9ee37b05338a676f90ee559465723d8b438f91ac
Milan Svehla <milan.svehla@gmail.com>
2018-06-22 23:36:25 +0200

    multiple formats of month_id

--- pretty-month.php
+++ pretty-month.php
@@ -1,12 +1,14 @@
+<?
+/**
+ * @param $month_id 01-12 or 1-12
+*/
 function pretty_month($month_id) {
 
-	if($month_id > 9) {
-		$month = ltrim($month_id, 1);
-	} else {
-		$month = $month_id;
-	}
+	$month = intval($month_id);
 
 	$months = ["ledna", "února", "března", "dubna", "května", "června", "července", "srpna", "září", "října", "listopadu", "prosince"];
 
 	return $months[$month_id - 1];
 }
+
+?>
