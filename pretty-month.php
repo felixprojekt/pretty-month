a461688f6fd3fdb6aa54b61e01df66f4dcda46af
Milan Svehla <milan.svehla@gmail.com>
2018-07-25 14:16:00 +0200

    inflection added

--- pretty-month.php
+++ pretty-month.php
@@ -2,11 +2,16 @@
 /**
  * @param $month_id 01-12 or 1-12
 */
-function pretty_month($month_id) {
+function pretty_month($month_id, $inflection = false) {
 
 	$month = intval($month_id);
 
-	$months = ["ledna", "února", "března", "dubna", "května", "června", "července", "srpna", "září", "října", "listopadu", "prosince"];
+	if($inflection) {
+		$months = ["ledna", "února", "března", "dubna", "května", "června", "července", "srpna", "září", "října", "listopadu", "prosince"];
+	} else {
+		$months = ["leden", "únor", "březen", "duben", "květen", "červen", "červenec", "srpen", "září", "říjen", "listopad", "prosinec"];
+	}
+	
 
 	return $months[$month - 1];
 }
