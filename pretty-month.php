6873533d94a2e6f566af24d67c791dcb32f4db04
Milan Svehla <milan.svehla@gmail.com>
2018-07-25 14:17:52 +0200

    phpdoc created

--- pretty-month.php
+++ pretty-month.php
@@ -1,19 +1,22 @@
 <?
 /**
- * @param $month_id 01-12 or 1-12
-*/
+ * @param $month_id
+ * @param bool $inflection
+ * @return mixed
+ * @internal $month_id can be can be 01-12 or 1-12
+ */
 function pretty_month($month_id, $inflection = false) {
 
-	$month = intval($month_id);
+    $month = intval($month_id);
 
-	if($inflection) {
-		$months = ["ledna", "února", "března", "dubna", "května", "června", "července", "srpna", "září", "října", "listopadu", "prosince"];
-	} else {
-		$months = ["leden", "únor", "březen", "duben", "květen", "červen", "červenec", "srpen", "září", "říjen", "listopad", "prosinec"];
-	}
-	
+    if($inflection) {
+        $months = ["ledna", "února", "března", "dubna", "května", "června", "července", "srpna", "září", "října", "listopadu", "prosince"];
+    } else {
+        $months = ["leden", "únor", "březen", "duben", "květen", "červen", "červenec", "srpen", "září", "říjen", "listopad", "prosinec"];
+    }
 
-	return $months[$month - 1];
+
+    return $months[$month - 1];
 }
 
 ?>
